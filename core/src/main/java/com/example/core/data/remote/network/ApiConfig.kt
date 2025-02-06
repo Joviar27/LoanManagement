b71039389f5247e76629e00af57936cd03d55228
Joviar <joviar27@gmail.com>
2025-02-06 10:30:52 +0700

    feat: use normal okhttp client

--- core/src/main/java/com/example/core/data/remote/network/ApiConfig.kt
+++ core/src/main/java/com/example/core/data/remote/network/ApiConfig.kt
@@ -5,11 +5,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 object ApiConfig {
     private var BASE_URL : String = BuildConfig.BASE_URL
@@ -29,32 +24,9 @@ object ApiConfig {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(getUnsafeOkHttpClient(interceptor))
+            .client(client)
             .build()
 
         return retrofit.create(ApiService::class.java)
-    }
-
-    private fun getUnsafeOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
-        try {
-            val trustAllCerts = arrayOf<TrustManager>(
-                object : X509TrustManager {
-                    override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-                    override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-                }
-            )
-
-            val sslContext = SSLContext.getInstance("TLS")
-            sslContext.init(null, trustAllCerts, SecureRandom())
-
-            return OkHttpClient.Builder()
-                .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
-                .hostnameVerifier { _, _ -> true }
-                .addInterceptor(interceptor)
-                .build()
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
     }
 }
